cc2090aab0e7723d8c6aa4335bf528161d1389ec
Jinsen <jinsen47@gmail.com>
2015-03-07 16:46:44 +0800

    Migrate from Bluetooth Chat

--- BluetoothFinder/app/src/main/java/com/jinsen/bluetoothfinder/MainActivity.java
+++ BluetoothFinder/app/src/main/java/com/jinsen/bluetoothfinder/MainActivity.java
@@ -1,17 +1,104 @@
 package com.jinsen.bluetoothfinder;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    // Debugging
+    private static final String TAG = "BluetoothChat";
+    private static final boolean D = true;
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    //Toast Text
+    private static final String BT_INVALID = "蓝牙不可用";
+
+    // Layout Views
+    private Spinner mDeviceSpinner;
+    private RadioGroup mGroup;
+    private Spinner mAlarmSpinner;
+    private ImageButton mSendButton;
+
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // Array adapter for the conversation thread
+    private ArrayAdapter<String> mConversationArrayAdapter;
+    // String buffer for outgoing messages
+    private StringBuffer mOutStringBuffer;
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for the chat services
+    private BluetoothChatService mChatService = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(D) Log.e(TAG, "+++ ON CREATE +++");
+
         setContentView(R.layout.activity_main);
+
+        //Get views' reference
+        mDeviceSpinner = ((Spinner) findViewById(R.id.devicespinner));
+        mGroup = ((RadioGroup) findViewById(R.id.radiogroup));
+        mAlarmSpinner = ((Spinner) findViewById(R.id.alarmspinner));
+        mSendButton = ((ImageButton) findViewById(R.id.sendButton));
+
+        //Request Bluetooth
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            showText(BT_INVALID);
+            finish();
+            return;
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(D) Log.e(TAG, "++ ON START ++");
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else {
+            if (mChatService == null) ;
+        }
     }
 
 
@@ -35,5 +122,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showText(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
     }
 }
